a1ceeaf515f38c9d08084756b672009730fc161b
Lyzander Marciano Andrylie <lyzanderandrylie@gmail.com>
2024-02-22 11:14:19 +0700

    Latihan: Implementasi Physics Sederhana (Gravitasi dan Loncat)

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -1,16 +1,22 @@
 extends KinematicBody2D
 
 export (int) var speed = 400
+export (int) var GRAVITY = 1200
+export (int) var jump_speed = -600
 
 var velocity = Vector2()
+const UP = Vector2(0,-1)
 
 func get_input():
-	velocity = Vector2()
+	velocity.x = 0
+	if is_on_floor() and Input.is_action_just_pressed('up'):
+		velocity.y = jump_speed
 	if Input.is_action_pressed('right'):
 		velocity.x += speed
 	if Input.is_action_pressed('left'):
 		velocity.x -= speed
 
 func _physics_process(delta):
+	velocity.y += delta * GRAVITY
 	get_input()
-	velocity = move_and_slide(velocity)
+	velocity = move_and_slide(velocity, UP)
